25f9656628febf3b5f0785d9c73972e23e7e7ac9
gordonjun2 <gordonoh@yahoo.com.sg>
2023-03-04 00:30:14 +0800

    continue with Solana Core-4

--- buildspace/Solana-Core/Core-3/2. Native-Solana-Development/solana-movie-and-student-client/src/core4ShipChallenge.ts
+++ buildspace/Solana-Core/Core-3/2. Native-Solana-Development/solana-movie-and-student-client/src/core4ShipChallenge.ts
@@ -33,9 +33,20 @@ const studentIntroLayout = borsh.struct([
     borsh.str('message'),
 ])
 
+const studentIntroReplyLayout = borsh.struct([
+    borsh.u8('variant'),
+    borsh.str('reply'),
+])
+
+const studentIntroReplyCounterSchema = borsh.struct([
+    borsh.str("discriminator"),
+    borsh.bool("isInitialized"),
+    borsh.u64("counter"),
+  ])
+
 async function sendTestStudentIntro(signer: web3.Keypair, programId: web3.PublicKey, connection: web3.Connection) {
     let buffer = Buffer.alloc(1000)
-    const studentName = 'Bibi.clone()'
+    const studentName = 'Bibi.clone(1)'
     studentIntroLayout.encode(
         {
             variant: 0,
@@ -47,16 +58,23 @@ async function sendTestStudentIntro(signer: web3.Keypair, programId: web3.Public
 
     buffer = buffer.slice(0, studentIntroLayout.getSpan(buffer))
 
-    const [pda] = await web3.PublicKey.findProgramAddress(
+    const [user_account_pda] = await web3.PublicKey.findProgramAddress(
         [signer.publicKey.toBuffer(), Buffer.from(studentName)],
         programId
     )
 
-    console.log("PDA is:", pda.toBase58())
+    console.log("User Account PDA is:", user_account_pda.toBase58())
 
-    const transaction = new web3.Transaction()
+    const [reply_counter_pda] = await web3.PublicKey.findProgramAddress(
+        [user_account_pda.toBuffer(), Buffer.from('reply')],
+        programId
+    )
+
+    console.log("Reply Counter PDA is:", reply_counter_pda.toBase58())
+
+    let transaction = new web3.Transaction()
     
-    const instruction = new web3.TransactionInstruction({
+    const add_intro_instruction = new web3.TransactionInstruction({
         programId: programId,
         data: buffer,
         keys: [
@@ -66,7 +84,103 @@ async function sendTestStudentIntro(signer: web3.Keypair, programId: web3.Public
                 isWritable: false
             },
             {
-                pubkey: pda,
+                pubkey: user_account_pda,
+                isSigner: false,
+                isWritable: true
+            },
+            {
+                pubkey: web3.SystemProgram.programId,
+                isSigner: false,
+                isWritable: false
+            },
+            {
+                pubkey: reply_counter_pda,
+                isSigner: false,
+                isWritable: true
+            }
+        ]
+    })
+
+    transaction.add(add_intro_instruction)
+
+    buffer = Buffer.alloc(1000)
+    studentIntroLayout.encode(
+        {
+            variant: 1,
+            name: studentName,
+            message: 'I am learning Solana development and I blove Pipi!' 
+        },
+        buffer
+    )
+
+    buffer = buffer.slice(0, studentIntroLayout.getSpan(buffer))
+
+    const update_intro_instruction = new web3.TransactionInstruction({
+        programId: programId,
+        data: buffer,
+        keys: [
+            {
+                pubkey: signer.publicKey,
+                isSigner: true,
+                isWritable: false
+            },
+            {
+                pubkey: user_account_pda,
+                isSigner: false,
+                isWritable: true
+            }
+        ]
+    })
+
+    transaction.add(update_intro_instruction)
+
+    let tx = await web3.sendAndConfirmTransaction(connection, transaction, [signer])
+    console.log(`https://explorer.solana.com/tx/${tx}?cluster=devnet`)
+
+    transaction = new web3.Transaction()
+
+    buffer = Buffer.alloc(1000)
+    studentIntroReplyLayout.encode(
+        {
+            variant: 2,
+            reply: 'That is so cool...'
+        },
+        buffer
+    )
+
+    buffer = buffer.slice(0, studentIntroReplyLayout.getSpan(buffer))
+
+    const reply_counter_account = await connection.getAccountInfo(reply_counter_pda)
+    let commentCount = studentIntroReplyCounterSchema.decode(reply_counter_account?.data).counter
+
+    const [reply_pda] = await web3.PublicKey.findProgramAddress(
+        [user_account_pda.toBuffer(), Buffer.from(commentCount.toArrayLike(Buffer, "be", 8))],
+        programId
+    )
+
+    console.log("Reply PDA is:", reply_pda.toBase58())
+
+    const add_reply_instruction = new web3.TransactionInstruction({
+        programId: programId,
+        data: buffer,
+        keys: [
+            {
+                pubkey: signer.publicKey,
+                isSigner: true,
+                isWritable: false
+            },
+            {
+                pubkey: user_account_pda,
+                isSigner: false,
+                isWritable: true
+            },
+            {
+                pubkey: reply_counter_pda,
+                isSigner: false,
+                isWritable: true
+            },
+            {
+                pubkey: reply_pda,
                 isSigner: false,
                 isWritable: true
             },
@@ -78,18 +192,19 @@ async function sendTestStudentIntro(signer: web3.Keypair, programId: web3.Public
         ]
     })
 
-    transaction.add(instruction)
-    const tx = await web3.sendAndConfirmTransaction(connection, transaction, [signer])
+    transaction.add(add_reply_instruction)
+
+    tx = await web3.sendAndConfirmTransaction(connection, transaction, [signer])
     console.log(`https://explorer.solana.com/tx/${tx}?cluster=devnet`)
 }
 
 async function main() {
     const signer = initializeSignerKeypair()
     
-    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
+    const connection = new web3.Connection('http://127.0.0.1:8899')
     await airdropSolIfNeeded(signer, connection)
     
-    const studentIntroProgramId = new web3.PublicKey('EuRX3XHYNPAUezFB6h9Vep1D2hCDMXo6aKsirnhkckEv')
+    const studentIntroProgramId = new web3.PublicKey('AnTuV637hoXxWsWbW8xzXCaTSLLEuWEVaxLxG6bmfww1')
     await sendTestStudentIntro(signer, studentIntroProgramId, connection)
 }
 
